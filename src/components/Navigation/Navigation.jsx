52d0d3f6a800e23737f66f8ebac5b1494127edad
livefromthelevel <brian.d.wach@gmail.com>
2024-05-04 16:32:39 -0500

    Set up router navigation for different pages for content

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,50 @@
+import { Link, useLocation } from 'react-router-dom';
 import './Navigation.css';
 
 function Navigation() {
-    return (<h2>Navigation Links</h2>);
+    const currentPage = useLocation().pathname;
+
+    return (
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <Link
+                    to="/"
+                    // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+                    className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+                >
+                    About
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to="/Portfolio"
+                    // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+                    className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
+                >
+                    Portfolio
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to="/Contact"
+                    // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+                    className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+                >
+                    Contact
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to="/Resume"
+                    // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+                    className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+                >
+                    Resume
+                </Link>
+            </li>
+        </ul>
+    );
 }
+
 
 export default Navigation;
