d6b60eeeedbe09274272731413e6e7bb8748fdca
livefromthelevel <brian.d.wach@gmail.com>
2024-05-07 21:39:31 -0500

    Moved image static assets to public folder so they would get included in the build

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -3,28 +3,28 @@ const projectData = [
         name: 'Concert Sampler',
         appUrl: 'https://magicaryn.github.io/ConcertSampler/',
         githubUrl: 'https://github.com/Magicaryn/ConcertSampler',
-        image: 'src/images/concertsampler.png',
+        image: '/images/concertsampler.png',
         alt: 'Screenshot of Concert Sampler application'
     },
     {   
         name: 'JavaScript Quiz',
         appUrl: 'https://briandwach.github.io/codingassessment',
         githubUrl: 'https://github.com/briandwach/codingassessment',
-        image: 'src/images/javascriptquiz.png',
+        image: '/images/javascriptquiz.png',
         alt: 'Screenshot of JavaScript quiz application'
     },
     {   
         name: 'Text Editor PWA',
         appUrl: 'https://text-editor-i76a.onrender.com/',
         githubUrl: 'https://github.com/briandwach/text-editor-pwa',
-        image: 'src/images/texteditorpwa.png',
+        image: '/images/texteditorpwa.png',
         alt: 'Screenshot of text editor progressive web application'
     },
     {   
         name: 'Work Day Scheduler',
         appUrl: 'https://briandwach.github.io/workdayscheduler/',
         githubUrl: 'https://github.com/briandwach/workdayscheduler',
-        image: 'src/images/workdayscheduler.png',
+        image: '/images/workdayscheduler.png',
         alt: 'Screenshot of workday scheduler application'
     }
 ];
