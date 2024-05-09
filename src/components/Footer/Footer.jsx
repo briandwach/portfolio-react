2019d48ca230105f0d5e6a88c980349dfdbcc5bb
livefromthelevel <brian.d.wach@gmail.com>
2024-05-08 22:49:07 -0500

    Added footer icons and links for github, LinkedIn, and SoundCloud

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,18 @@
 import './Footer.css';
 
 function Footer() {
-    return (<h1>Footer</h1>);
+    return (<div className="footerContainer">
+        <a href="https://github.com/briandwach" target="_blank" rel="noopener noreferrer">
+            <img className="logoFooter" src='/images/github-mark.png' alt='Github Invertocat logo' />
+        </a>
+        <a href="https://www.linkedin.com/in/brianwach/" target="_blank" rel="noopener noreferrer">
+            <img className="logoFooter" src='/images/In-Blue-128.png' alt='LinkedIn blue In logo' />
+        </a>
+        <a href="https://soundcloud.com/afda-glo" target="_blank" rel="noopener noreferrer">
+            <img className="logoFooter" src='/images/SoundCloudLogo.jpg' alt='LinkedIn blue In logo' />
+        </a>
+    </div>
+    );
 }
- 
+
 export default Footer;
