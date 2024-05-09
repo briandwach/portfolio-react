f21fc0761666b850a96e2eeb0256a793d438a2db
livefromthelevel <brian.d.wach@gmail.com>
2024-05-08 23:30:30 -0500

    Content for resume, including link to resume pdf

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,31 @@
 export default function Resume() {
     return (
       <div>
-        <h1>Resume Page</h1>
+        <h1>Resume</h1>
         <p>
-          Details
+          Download my <a href='/images/wachresume.pdf' target="_blank" rel="noopener noreferrer">resume</a>
         </p>
+        
+      <h3>Front-end Proficiencies</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>jQuery</li>
+        <li>Responsive Design</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
+
+      <h3>Back-end Proficiencies</h3>
+      <ul>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>PostgreSQL, Sequelize</li>
+        <li>MongoDB, Mongoose</li>
+        <li>graphQL</li>
+      </ul>
       </div>
     );
   }
