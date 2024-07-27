6a2e4e666d389a3a861c66d9eb642dc8afad4160
livefromthelevel <brian.d.wach@gmail.com>
2024-07-27 18:21:32 -0500

    7.27.24/Tailwind/Added Tailwind directives to index.css file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,11 +5,10 @@ export default function About() {
       <div className="aboutContent">
         <img className="profilePicture" src='/images/headshot.png' alt='Headshot of Brian Wach' />
         <p>
-          Brian Wach is an audio-visual technician, musician, and composer in the Chicagoland area.
-          He completed his master’s degree in Recording Arts and earned a bachelor’s in Percussion Performance, both at Northern Illinois University (NIU).
-          Brian strives to combine his passion for technology and music in order to create inspiring and meaningful experiences for others.
-          Since 2014, he has been working in event production and information technologies as a graduate assistant at NIU, audio warehouse manager with Sound Investment, and freelance streaming technician/virtual meeting producer for Brella Productions and Intempio.
-          Before the current health pandemic, Brian could also be found playing/recording music with his acoustic duo, Off the Curb, sound engineering for the Emanation Music Collective, and performing with the improv comedy team, All Caps.
+          Full stack web developer with a background in music and audio visual technologies.
+        </p>
+        <p>
+          Focused on creating intuitive web applications and products to serve the needs of businesses and people.
         </p>
       </div>
     </div>
