3888783478d55ce7cf9de46bc6cb665ad68353d8
livefromthelevel <brian.d.wach@gmail.com>
2024-07-28 20:30:16 -0500

    7.28.24/Resume/Updated stylings for resume page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,9 @@ function Header() {
     return (
         <div className="bg-">
             <div className='m-2'>
-            <Link className='text-5xl text-base-content desktop:hover:text-accent desktop:active:text-success' to='/'>
-            Brian Wach
+            <Link className='font-bold text-base-content desktop:hover:text-accent desktop:active:text-success' to='/'>
+            <h1 className='text-5xl'>Brian Wach</h1>
+            <h1 className='ml-[2px]'>Full Stack Web Developer</h1>
              </Link>
              </div>
             <Navigation />
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,14 +1,17 @@
 export default function Resume() {
   return (
     <div>
-      <h1>Resume</h1>
-      <p>
-        Download my <a className="resumeLink" href='/images/wachresume.pdf' target="_blank" rel="noopener noreferrer">resume</a>
+      <h1 className='text-3xl m-2'>Resume</h1>
+      <p className='m-2 mb-5'>
+        Download my <a href='/images/wachresume.pdf' target="_blank" rel="noopener noreferrer"
+        className='text-accent desktop:hover:text-success desktop:active:text-base-content'>
+        resume
+        </a>
       </p>
 
-      <div className="skills">
-        <h3>Front-end Proficiencies</h3>
-        <ul>
+      <div className="m-[10px]">
+        <h3 className='text-2xl font-bold'>Front-end Proficiencies</h3>
+        <ul className='list-disc mb-8 ml-4'>
           <li>HTML</li>
           <li>CSS</li>
           <li>JavaScript</li>
@@ -18,8 +21,8 @@ export default function Resume() {
           <li>Bootstrap</li>
         </ul>
 
-        <h3>Back-end Proficiencies</h3>
-        <ul>
+        <h3 className='text-2xl font-bold'>Back-end Proficiencies</h3>
+        <ul className='list-disc mb-8 ml-4'>
           <li>APIs</li>
           <li>Node</li>
           <li>Express</li>
