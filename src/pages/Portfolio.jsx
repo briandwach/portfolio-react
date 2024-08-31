5611213e05bcc88cf1389885ad64197116833487
livefromthelevel <brian.d.wach@gmail.com>
2024-08-31 17:29:35 -0500

    Fixed bug where placholder was not maintaining aspect ratio on mobile

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,7 +5,7 @@ export default function Portfolio() {
   return (
       <div>
           <h1 className='text-3xl m-2 center:w-fit center:mx-auto'>Projects</h1>
-          <div className='desktop:flex desktop:flex-wrap desktop:justify-center flex flex-wrap justify-center'>
+          <div className='flex flex-wrap justify-center'>
               {projectData.map((project) => (
                   <Project
                       key={project.name}
